108f28a13738a6f9fd99000bba4ce6f8a8ad7a25
lybimaisa <zanfurry2006@gmail.com>
2024-09-21 17:23:33 +0300

    Lab-1

---
+++ Lab-1/Lab-1/Lab-1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	bool isHuman = false;
+	if (argc <= 1 || strcmp(argv[1], "false") != 0)
+	{
+		isHuman = true;
+	}
+	if (isHuman)
+	{
+		double x, y;
+		setlocale(LC_ALL, "");
+		std::wcout << L"Введите координаты точки:" << std::endl;
+		std::cin >> x >> y;
+		if (0 <= x && x <= 3)
+		{
+			if (y <= (-4 * x / 3))
+			{
+				std::wcout << L"Точка входит в треугольник." << std::endl;
+			}
+			else
+			{
+				std::wcout << L"Точка не входит в треугольник." << std::endl;
+			}
+		}
+		else
+		{
+			std::wcout << L"Точка не входит в треугольник." << std::endl;
+		}
+	}
+	else
+	{
+		double x, y;
+		std::cin >> x >> y;
+		if (0 <= x && x <= 3)
+		{
+			if (y <= (-4 * x / 3))
+			{
+				std::cout << "YES";
+			}
+			else
+			{
+				std::cout << "NO";
+			}
+		}
+		else
+		{
+			std::cout << "NO";
+		}
+	}
+}
