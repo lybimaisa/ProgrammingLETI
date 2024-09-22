3419dd07ffcc198ff341fc5c3a49d0288315157d
lybimaisa <zanfurry2006@gmail.com>
2024-09-22 19:30:33 +0300

    Update Lab-1.cpp

--- Lab-1/Lab-1/Lab-1.cpp
+++ Lab-1/Lab-1/Lab-1.cpp
@@ -7,75 +7,77 @@ int main(int argc, char* argv[])
 	{
 		isHuman = true;
 	}
+	double x, y;
 	if (isHuman)
 	{
-		double x, y;
 		setlocale(LC_ALL, "");
 		std::wcout << L"Дан треугольник с координатами вершин (0, 0) (3, 0) (0, 4)." << std::endl;
 		std::wcout << L"Чтобы проверить, находится ли точка в треугольнике, введите через пробел сначала координату оси x, а затем оси y." << std::endl;
-		do
+	}
+	do
+	{
+		if (isHuman)
 		{
 			std::wcout << L"Введите координаты точки:" << std::endl;
-			std::cin >> x >> y;
-			if (!std::cin)
+		}
+		std::cin >> x >> y;
+		if (!std::cin)
+		{
+			if (isHuman)
 			{
 				std::wcout << L"Введённые данные не являются числом." << std::endl;
-				break;
 			}
-			if (x == 0 and y == 0)
+			break;
+		}
+		if (x == 0 and y == 0)
+		{
+			break;
+		}
+		if (0 <= x && x <= 3)
+		{
+			if (0 <= y && y <= (-4 * x / 3 + 4))
 			{
-				break;
-			}
-			if (0 <= x && x <= 3)
-			{
-				if (0 <= y && y <= (-4 * x / 3 + 4))
+				if (isHuman)
 				{
 					std::wcout << L"Точка входит в треугольник." << std::endl;
 				}
 				else
 				{
-					std::wcout << L"Точка не входит в треугольник." << std::endl;
+					std::cout << "YES" << std::endl;
 				}
 			}
 			else
 			{
-				std::wcout << L"Точка не входит в треугольник." << std::endl;
-			}
-		} while (x != 0 or y != 0);
-		std::wcout << L"Конец программы." << std::endl;
-		system("pause");
-		return 0;
-	}
-	else
-	{
-		double x, y;
-		do
-		{
-			std::cin >> x >> y;
-			if (!std::cin)
-			{
-				break;
-			}
-			if (x == 0 and y == 0)
-			{
-				break;
-			}
-			if (0 <= x && x <= 3)
-			{
-				if (0 <= y && y <= (-4 * x / 3 + 4))
+				if (isHuman)
 				{
-					std::cout << "YES" << std::endl;
+					std::wcout << L"Точка не входит в треугольник." << std::endl;
 				}
 				else
 				{
 					std::cout << "NO" << std::endl;
 				}
 			}
+		}
+		else
+		{
+			if (isHuman)
+			{
+				std::wcout << L"Точка не входит в треугольник." << std::endl;
+			}
 			else
 			{
 				std::cout << "NO" << std::endl;
 			}
-		} while (x != 0 or y != 0);
+		}
+	} while (x != 0 or y != 0);
+	if (isHuman)
+	{
+		std::wcout << L"Конец программы." << std::endl;
+		system("pause");
+		return 0;
 	}
-	return 0;
+	else
+	{
+		return 0;
+	}
 }
